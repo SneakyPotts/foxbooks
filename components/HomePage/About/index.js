77613a15630ced6787c58d8f5cbbfb041b58f3af
Bond-Anna <anna93bondarenko@gmail.com>
2021-12-08 17:07:37 +0200

    fix

--- components/HomePage/About/index.js
+++ components/HomePage/About/index.js
@@ -1,5 +1,6 @@
-// import DropDownBooksOnline from "./Blocks/dropDownBooksOnline"
-    import css from './about.module.css'
+import { useState } from 'react'
+import DropDownArrow from '../../../public/chevron-down.svg'
+import css from './about.module.css'
 
 const About = () => {
     const data = [
@@ -7,12 +8,26 @@ const About = () => {
         { title: 'Книги онлайн', text: 'Понимание сути ресурсосберегающих технологий влечет за собой процесс внедрения и модернизации благоприятных перспектив.Современные технологии достигли такого уровня, что выбранный нами инновационный путь прекрасно подходит для реализации соответствующих условий активизации.В целом, конечно, новая модель организационной деятельности влечет за собой процесс внедрения и модернизации глубокомысленных рассуждений.' },
         { title: 'Книги на IPhone, IPad и Android онлайн', text: 'Понимание сути ресурсосберегающих технологий влечет за собой процесс внедрения и модернизации благоприятных перспектив.Современные технологии достигли такого уровня, что выбранный нами инновационный путь прекрасно подходит для реализации соответствующих условий активизации.В целом, конечно, новая модель организационной деятельности влечет за собой процесс внедрения и модернизации глубокомысленных рассуждений.' }
     ]
+
+        const [showText, setShowText] = useState(false);
+
+
+    const onBtnClick = () => {
+        setShowText(prevSetShowText=>!prevSetShowText)
+    }
     return (
         <div className={css.container}>
-            qweqweqweqweqweqweqwewqe
-            {/* {data.map(({title, text}) => (
-                <DropDownBooksOnline key={title} title={title} text={text} />
-            ))} */}
+         <div className={classnames(css.dropDown, { [css.active]: showText })}>
+        <button onClick={onBtnClick} className={css.dropDownBtn}>
+            <span className={css.dropDownTitle}>{title}</span>
+            <span className={classnames(css.dropDownIcon, { [css.activeBtn]: showText })}>
+                <DropDownArrow />
+            </span>
+            </button >
+            {showText &&
+                <p className={css.dropDownText}>{text}</p>
+            }
+        </div>
         </div>
     )
 }
