3fce4b137e5b263380cc6cb0d8139cacd42fc5ea
dezatop <toxashark@gmail.com>
2021-12-09 15:50:07 +0200

    add star rating and add audio books component

--- components/HomePage/Introductory block/AudioBooks/AudioBooks.js
+++ components/HomePage/Introductory block/AudioBooks/AudioBooks.js
@@ -2,14 +2,38 @@ import Link from 'next/link';
 import ArrowRight from '../../../../public/chevron-right.svg'
 import css from './audioBooks.module.css'
 
+import {Navigation} from 'swiper/core';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css/bundle';
+import Book from "../../../book";
+
 const AudioBooks = () => {
-    return <div className={css.container}>
-        <h2 className={css.title}>Некогда читать - слушайте!</h2>
-        <Link href="/audioBooks">
-              <a className={css.newLink}>
-                Смотреть все <ArrowRight className={css.arrowRight}/>
-              </a>
-            </Link>
-    </div>
+
+    const testData = [{id: '1'}, {id: '1'}, {id: '1'}, {id: '1'}, {id: '1'}, {id: '1'}, {id: '1'}, {id: '1'}]
+
+    return (
+        <div className={css.container}>
+            <div className={css.wrapHeader}>
+                <h2 className={css.title}>Некогда читать - слушайте!</h2>
+                <Link href="/audioBooks">
+                    <a className={css.newLink}>
+                        Смотреть все <ArrowRight className={css.arrowRight}/>
+                    </a>
+                </Link>
+            </div>
+            <Swiper
+                spaceBetween={24}
+                slidesPerView={6}
+            >
+                {testData.map((r,i) => {
+                    return (
+                            <SwiperSlide key={i}>
+                                <Book/>
+                            </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </div>
+    )
 }
 export default AudioBooks
--- components/book/index.js
+++ components/book/index.js
@@ -1,36 +1,48 @@
 import Image from 'next/image';
-
+import ReactStars from "react-rating-stars-component";
 import css from './book.module.css';
 
-const Book = ({ book }) => {
-const secondExample = {
-  size: 50,
-  count: 10,
-  color: "black",
-  activeColor: "red",
-  value: 7.5,
-  a11y: true,
-  isHalf: true,
-  emptyIcon: <i className="far fa-star" />,
-  halfIcon: <i className="fa fa-star-half-alt" />,
-  filledIcon: <i className="fa fa-star" />,
-  onChange: newValue => {
-    console.log(`Example 2: new value is ${newValue}`);
-  }
-};
-    return <div className={css.wrapper}>
-        <Image
-            src={book.img}
-            alt=""
-            width="180"
-            height="271"
-            placeholder="blur"
-            blurDataURL="/images/blur.jpg"
-        />
-        <div className={css.bookRating}><span>{book.rating}</span></div>
-        <h3 className={css.bookName}>{book.name}</h3>
-        <p className={css.bookAuthor}>{book.author}</p>
-        <span className={css.bookCategorie}>{ book.categorie}</span>
-    </div>
+
+const Book = ({book}) => {
+
+    const secondExample = {
+        size: 15,
+        count: 5,
+        value: 2.5,
+        color: "#D5D5D5",
+        activeColor: "#FEC420",
+        edit: false,
+        // a11y: true,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />
+        // onChange: (newValue) => {
+        //     setRating(newValue);
+        // }
+    };
+    return (
+        <div className={css.wrapper}>
+            <Image
+                src='/horizontalBookCovers/book.png'
+                alt=""
+                width="180"
+                height="271"
+                placeholder="blur"
+                blurDataURL="/images/blur.jpg"
+            />
+            <div className={css.bookRating}>
+                <ReactStars {...secondExample} />
+                <div>
+                    <span>4,9 </span>
+                    <span>(450)</span>
+                </div>
+
+            </div>
+            <h3 className={css.bookName}>Искатели неба: Холодные берега Искатели неба: Холодные берега</h3>
+            <p className={css.bookAuthor}>Сергей Лукьяненко</p>
+            <span className={css.bookCategorie}>Фентези</span>
+        </div>
+    )
 }
 export default Book
