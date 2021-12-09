01a8886361e35f6b0da5d34ee7379856b5fe3174
Bond-Anna <anna93bondarenko@gmail.com>
2021-12-09 18:05:16 +0200

    add reviews

--- components/HomePage/Introductory block/Reviews/Reviews.js
+++ components/HomePage/Introductory block/Reviews/Reviews.js
@@ -4,18 +4,20 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/bundle';
 import { Navigation } from 'swiper/core';
 import classnames from "classnames";
+import Eye from "../../../shared/icons/eye";
 import ArrowRight from '../../../../public/chevron-right.svg'
-
-import css from './reviews.module.scss'
+import Like from '../../../shared/icons/heart';
+import Comment from '../../../shared/icons/comment';
+import dataReview from '../../../data/reviews.json'
+import st from './reviews.module.scss'
 
 
 const Reviews = () => {
-  const data = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }, { id: '6' },{ id: '7' }, { id: '8' }]
   
-  return <> <div className={css.containerReviews}>
-    <h2 className={css.title}>Рецензии</h2>
+  return <> <div className={st.containerReviews}>
+    <h2 className={st.title}>Рецензии</h2>
     <Link href="/reviews">
-      <a className={css.newLink}>
+      <a className={st.newLink}>
         Смотреть все <ArrowRight />
       </a>
     </Link>
@@ -30,13 +32,34 @@ const Reviews = () => {
         slidesPerView={3}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}>
-      {data.map(id => (<SwiperSlide key={id}>
-        <div className={css.reviewsCover}><Image src="/reviewsBookCovers/cover1.png" alt="" width="86" height="143" /></div>
+      {dataReview.map(review => (<SwiperSlide key={review.id}>
+        <div className={st.reviewsCover}>
+          <Image src={review.img} alt="" width="86" height="143" />
+          <div className={st.bookMainInfo}>
+            <h3 className={st.bookTitle}>{review.book_title}</h3>
+            <p className={st.bookAuthor}>{review.book_author}</p>
+          </div>
+        </div>
+        <div className={st.reviewInfo}>
+          <p className={st.reviewData}>{review.review_data}</p>
+          <div className={st.reviewViews}>
+            <span className={st.sumReviews}>{review.sum_reviews}</span>
+            <Eye />
+          </div>
+        </div>
+        <p>{review.review_title}</p>
+        <p>{review.review_text}</p>
+        <div>
+          <Like className={st.reviewIcon}/>
+          <span className={st.reviewLike}>{review.likes}</span>
+          <Comment className={st.reviewIcon}/>
+          <span>{review.comments}</span>
+        </div>
       </SwiperSlide>))}
-        <button className={classnames("prevArrow", 'btnBefore')}>
+        <button className={classnames("prevArrow", st.btn)}>
           <ArrowRight className='arrowNext' />
         </button>
-        <button className={classnames("nextArrow", 'btnAfter')}>
+        <button className={classnames("nextArrow", st.btn)}>
           <ArrowRight className='arrowNext' />
         </button>
       </Swiper></>
