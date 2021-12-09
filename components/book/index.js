70739b4127488da00505fc70a0cc5dcf6d562c6c
Bond-Anna <anna93bondarenko@gmail.com>
2021-12-09 14:20:56 +0200

    no stars

--- components/book/index.js
+++ components/book/index.js
@@ -1,7 +1,23 @@
 import Image from 'next/image';
+
 import css from './book.module.css';
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
+const secondExample = {
+  size: 50,
+  count: 10,
+  color: "black",
+  activeColor: "red",
+  value: 7.5,
+  a11y: true,
+  isHalf: true,
+  emptyIcon: <i className="far fa-star" />,
+  halfIcon: <i className="fa fa-star-half-alt" />,
+  filledIcon: <i className="fa fa-star" />,
+  onChange: newValue => {
+    console.log(`Example 2: new value is ${newValue}`);
+  }
+};
     return <div className={css.wrapper}>
         <Image
             src={book.img}
@@ -11,7 +27,7 @@ const Book = ({book}) => {
             placeholder="blur"
             blurDataURL="/images/blur.jpg"
         />
-        <p className={css.bookRating}>{book.rating}</p>
+        <div className={css.bookRating}><span>{book.rating}</span></div>
         <h3 className={css.bookName}>{book.name}</h3>
         <p className={css.bookAuthor}>{book.author}</p>
         <span className={css.bookCategorie}>{ book.categorie}</span>
