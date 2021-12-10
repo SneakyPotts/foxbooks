bd5e80cc839c79be2293c06c4bfee76ae774eb81
dezatop <toxashark@gmail.com>
2021-12-10 15:58:43 +0200

    add modal window

--- components/shared/common/modalWindow/ModalWindow.jsx
+++ components/shared/common/modalWindow/ModalWindow.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import classnames from "classnames";
 import styles from './index.module.scss'
 
-const ModalWindow = ({children}) => {
+const ModalWindow = ({children, modal, setModal}) => {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.wrapperBlock}>
+        <div className={classnames(styles.wrapper, {[styles.wrapperActive]: modal})}>
+            <div onClick={(e)=> e.stopPropagation()} className={styles.wrapperBlock}>
 
-                <div className={styles.wrapperBlockCross}>
+                {children}
+
+                <div onClick={()=>setModal(!modal)} className={styles.wrapperBlockCross}>
                     <span/>
                     <span/>
                 </div>
