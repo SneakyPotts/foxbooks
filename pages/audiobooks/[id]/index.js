a3ba890799422a0b966156d44b6708cca7584ea0
dezatop <toxashark@gmail.com>
2021-12-15 08:03:25 +0200

    Fix double header

--- pages/audiobooks/[id]/index.js
+++ pages/audiobooks/[id]/index.js
@@ -1,18 +1,14 @@
-// import Breadcrumbs from '../../../components/BreadCrumps/BreadCrumps';
-import { useRouter } from 'next/router';
-import Header from '../../../components/Header';
 import Category from '../../../components/AudioBooks/category';
-import Footer from '../../../components/Footer';
+
 
 const Categories = () => {
-  const testete = useRouter();
-  console.log(testete, '555555555555555555555555');
 
-  return (
-    <>
-      {/* <Breadcrumbs data={breadcrumbsData} /> */}
-      <Category />
-    </>
-  );
+
+    return (
+        <>
+            {/* <Breadcrumbs data={breadcrumbsData} /> */}
+            <Category/>
+        </>
+    );
 };
 export default Categories;
--- pages/audiobooks/index.js
+++ pages/audiobooks/index.js
@@ -1,12 +1,11 @@
-import Header from '../../components/Header'
 import AudioBooks from '../../components/AudioBooks'
-import Footer from '../../components/Footer'
+
 
 const Audiobooks = () => {
-    return <>
-        <Header />
-        <AudioBooks/>
-        <Footer/>
-    </>
+    return (
+        <>
+            <AudioBooks/>
+        </>
+    )
 }
 export default Audiobooks
