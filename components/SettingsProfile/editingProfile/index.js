793031df0838cb1f933b69bbf6338a2933811003
Bond-Anna <anna93bondarenko@gmail.com>
2021-12-29 17:20:20 +0200

    select files

--- components/SettingsProfile/editingProfile/index.js
+++ components/SettingsProfile/editingProfile/index.js
@@ -1,101 +1,108 @@
-import React, {useRef, useState} from 'react';
-import styles from "./index.module.scss";
-import Image from "next/image";
-import Pencil from "../../shared/icons/pencil";
-import Input from "../../shared/common/Input/Input";
-import SocialNetwork from "../../shared/common/SocialNetwork/SocialNetwork";
-import {useForm} from "react-hook-form";
-import ButtonGroup from "../buttonGroup";
-import ModalWindow from "../../shared/common/modalWindow/ModalWindow";
+import React, { useRef, useState } from 'react';
+import styles from './index.module.scss';
+import Image from 'next/image';
+import Pencil from '../../shared/icons/pencil';
+import Input from '../../shared/common/Input/Input';
+import SocialNetwork from '../../shared/common/SocialNetwork/SocialNetwork';
+import { useForm } from 'react-hook-form';
+import ButtonGroup from '../buttonGroup';
+import ModalWindow from '../../shared/common/modalWindow/ModalWindow';
 
 const EditingProfile = () => {
+  const inputFile = useRef();
+  const [file64, setFile64] = useState(null);
+  const [modal, setModal] = useState(false);
 
-    const inputFile = useRef()
-    const [file64, setFile64] = useState(null)
-    const [modal, setModal] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    unregister,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-    const {register, handleSubmit, unregister, formState: {errors}, reset} = useForm();
+  const HandleClick = () => {
+    inputFile.current.click();
+  };
 
-    const HandleClick = () => {
-        inputFile.current.click()
-    }
-
-    const onChange = e => {
-        let reader = new FileReader();
-        const files = e.target.files;
-        reader.readAsDataURL(files[0]);
-        reader.onload = () => {
-            setFile64(reader.result)
-        };
+  const onChange = e => {
+    let reader = new FileReader();
+    const files = e.target.files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = () => {
+      setFile64(reader.result);
     };
+  };
 
-    const HandleSubmit = data => {
-        reset()
-        console.log(data)
-    }
+  const HandleSubmit = data => {
+    reset();
+    console.log(data);
+  };
 
-    return (
-        <>
-            <form
-                onSubmit={handleSubmit(HandleSubmit)}
-                className={styles.formProfile}>
-                <div  onClick={HandleClick} className={styles.formProfileWrap}>
-                    <div className={styles.image}>
-                        <Image
-                            src={file64 === null ? "/horizontalBookCovers/book.png" : file64}
-                            alt=""
-                            width="102"
-                            height='102'
-                            placeholder="blur"
-                            blurDataURL="/images/blur.jpg"
-                            layout='responsive'
-                        />
-                    </div>
-                    <div className={styles.file}>
-                        <Pencil w='20' h='20' c='#FFFFFF'/>
-                        <input onChange={onChange} ref={inputFile} type="file" hidden/>
-                    </div>
-                </div>
-                <Input
-                    classNames={styles.inputNik}
-                    err={errors.name?.message}
-                    textLabel='Ник'
-                    name='name'
-                    register={register}
-                />
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit(HandleSubmit)}
+        className={styles.formProfile}
+      >
+        <div onClick={HandleClick} className={styles.formProfileWrap}>
+          <div className={styles.image}>
+            <Image
+              src={file64 === null ? '/horizontalBookCovers/book.png' : file64}
+              alt=""
+              width="102"
+              height="102"
+              placeholder="blur"
+              blurDataURL="/images/blur.jpg"
+              layout="responsive"
+            />
+          </div>
+          <div className={styles.file}>
+            <Pencil w="20" h="20" c="#FFFFFF" />
+            <input onChange={onChange} ref={inputFile} type="file" hidden />
+          </div>
+        </div>
+        <Input
+          classNames={styles.inputNik}
+          err={errors.name?.message}
+          textLabel="Ник"
+          name="name"
+          register={register}
+        />
 
-                <Input
-                    err={errors.email?.message}
-                    textLabel='Электронная почта'
-                    name='email'
-                    register={register}
-                />
-                <div className={styles.social}>
-                    <span>Социальные сети</span>
-                    <p>
-                        Подключите социальные сети, чтобы входить через них в FoxBooks
-                    </p>
-                    <SocialNetwork ClassNames={styles.socialProfile} connect={true} title={false}/>
-                </div>
-                <ButtonGroup/>
-                <div onClick={()=>setModal(true)} className={styles.delProfile}>
-                    <span>Вы можете удалить свой профиль</span>
-                </div>
-            </form>
-            <ModalWindow modal={modal} setModal={setModal}>
-                <div className={styles.wrapDel}>
-                    <h2>Удалить профиль</h2>
-                    <p>
-                        Вы действительно хотите удалить профиль?
-                    </p>
-                    <ButtonGroup
-                        cancelClick={()=>setModal(false)}
-                        text='Удалить'
-                        ClassName={styles.Button}/>
-                </div>
-            </ModalWindow>
-        </>
-    );
+        <Input
+          err={errors.email?.message}
+          textLabel="Электронная почта"
+          name="email"
+          register={register}
+        />
+        <div className={styles.social}>
+          <span>Социальные сети</span>
+          <p>Подключите социальные сети, чтобы входить через них в FoxBooks</p>
+          <SocialNetwork
+            ClassNames={styles.socialProfile}
+            connect={true}
+            title={false}
+          />
+        </div>
+        <ButtonGroup />
+        <div onClick={() => setModal(true)} className={styles.delProfile}>
+          <span>Вы можете удалить свой профиль</span>
+        </div>
+      </form>
+      <ModalWindow modal={modal} setModal={setModal}>
+        <div className={styles.wrapDel}>
+          <h2>Удалить профиль</h2>
+          <p>Вы действительно хотите удалить профиль?</p>
+          <ButtonGroup
+            cancelClick={() => setModal(false)}
+            text="Удалить"
+            ClassName={styles.Button}
+          />
+        </div>
+      </ModalWindow>
+    </>
+  );
 };
 
 export default EditingProfile;
--- components/Support/Support.js
+++ components/Support/Support.js
@@ -1,32 +1,120 @@
-import React from 'react';
-import styles from './index.module.scss'
-import {useForm} from "react-hook-form";
-import Input from "../shared/common/Input/Input";
+import React, { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Image from 'next/image';
+import Input from '../shared/common/Input/Input';
+import ButtonGroup from '../SettingsProfile/buttonGroup';
+import Clip from '../../public/clip.svg';
+import styles from './index.module.scss';
 
 const SupportCom = () => {
+  const inputFile = useRef();
+  const [file64, setFile64] = useState(null);
+  const [imgGroup, setImgGroup] = useState([]);
 
-    const {register, handleSubmit, unregister, formState: {errors}, reset} = useForm();
+  let test = [];
 
-    const HandleSubmit = data => {
-        console.log(data)
+  const {
+    register,
+    handleSubmit,
+    unregister,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const HandleSubmit = data => {
+    console.log(data);
+  };
+
+  const HandleClick = () => {
+    inputFile.current.click();
+  };
+
+  //   renderListImages(arrayPhotoLoaded);
+
+  const generateBase64img = data => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      (function (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          setImgGroup([...imgGroup, { name: e.target.result }]);
+        };
+        reader.readAsDataURL(file);
+      })(data[i]);
     }
+  };
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.helpTitle}>
-                <h1>Нужна помощь? </h1>
-                <p>Если у вас появились вопросы или проблемы связанные с использованием сайта, напишите нашей службе
-                    поддержки, заполнив форму ниже. Ответ технических специалистов может занимать до 24-х часов.</p>
+  console.log(imgGroup);
+  console.log(test);
+  const onChange = e => {
+    let reader = new FileReader();
+    const files = Array.from(e.target.files);
+    generateBase64img(files);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.helpTitle}>
+        <h1>Нужна помощь? </h1>
+        <p>
+          Если у вас появились вопросы или проблемы связанные с использованием
+          сайта, напишите нашей службе поддержки, заполнив форму ниже. Ответ
+          технических специалистов может занимать до 24-х часов.
+        </p>
+      </div>
+      <form className={styles.form} onSubmit={handleSubmit(HandleSubmit)}>
+        <Input
+          classNames={styles.inputWidth}
+          register={register}
+          textLabel={'Тема обращения'}
+          name="message"
+        />
+        <Input
+          classNames={styles.inputWidth}
+          register={register}
+          textLabel={'Ваше имя'}
+          name="message"
+        />
+        <Input
+          classNames={styles.inputWidth}
+          register={register}
+          textLabel={'Электронная почта'}
+          name="message"
+        />
+        <div className={styles.inputArea}>
+          <textarea className={styles.textArea}></textarea>
+          <div onClick={HandleClick}>
+            <div className={styles.textAreaClip}>
+              <Clip />
+              <p>Файл</p>
             </div>
-            <form className={styles.form} onSubmit={handleSubmit(HandleSubmit)}>
-                <Input
-                    register={register}
-                    textLabel={'Тема обращения'}
-                    name='message'
-                />
-            </form>
+            {imgGroup?.map(r => {
+              return (
+                <>
+                  <img src={r.name} />
+                </>
+              );
+            })}
+            {/* {file64 ? (
+              <>
+              
+              </>
+            ) : null} */}
+
+            <input
+              multiple
+              onChange={onChange}
+              ref={inputFile}
+              type="file"
+              hidden
+            />
+          </div>
         </div>
-    );
+
+        <ButtonGroup ClassName={styles.buttons} text="Отправить" />
+      </form>
+    </div>
+  );
 };
 
 export default SupportCom;
