efce441dcaca066025da55ea1c220c7785a16dcb
Bond-Anna <anna93bondarenko@gmail.com>
2022-01-11 17:06:21 +0200

    add textArea

--- components/MyBooks/Edit/index.js
+++ components/MyBooks/Edit/index.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
+import Img from '../../../public/img.svg';
 import st from './edit.module.scss';
 import classnames from 'classnames';
 
@@ -23,22 +24,33 @@ const Edit = () => {
   };
 
   return (
-    <div className={st.dropblock} onClick={HandleClick}>
-      <div
-        className={classnames(st.dropblockImage, {
-          [st.dropblockImageActive]: file64,
-        })}
-        style={
-          file64 && {
-            backgroundImage: `url(${file64})`,
+    <div>
+      <div className={st.dropblock} onClick={HandleClick}>
+        <div
+          className={classnames(st.dropblockImage, {
+            [st.dropblockImageActive]: file64,
+          })}
+          style={
+            file64 && {
+              backgroundImage: `url(${file64})`,
+            }
           }
-        }
-      >
-        <div className={st.wrapper}>
-          <input placeholder="Название" />
+        >
+          <div className={st.wrapper}>
+            {!file64 && <Img className={st.imgSvg} />}
+            <input
+              placeholder="Название"
+              className={st.changeInput}
+              onClick={e => e.stopPropagation()}
+            />
+            <p className={st.changeInputLabel}>100 символов</p>
+          </div>
         </div>
+        <input onChange={onChange} ref={inputFile} type="file" hidden />
       </div>
-      <input onChange={onChange} ref={inputFile} type="file" hidden />
+      <div className="container">
+        <textarea className={st.replyArea}></textarea>
+      </div>
     </div>
   );
 };
