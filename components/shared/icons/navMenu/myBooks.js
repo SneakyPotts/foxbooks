c689b5d3aa089ad512c12069a02449dab01ec422
Bond-Anna <anna93bondarenko@gmail.com>
2022-01-18 10:59:20 +0200

    add icons to menu

---
+++ components/shared/icons/navMenu/myBooks.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const MyBooks = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.18066 3.34375V17.1405M9.19767 3.34375V17.1405M13.7966 3.34375L16.7232 17.1405"
+        stroke="inherit"
+        stroke-width="1.67233"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MyBooks;
