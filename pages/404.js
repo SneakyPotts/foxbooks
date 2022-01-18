6e425dffad4d68015170b0c39c22836737a3676f
Bond-Anna <anna93bondarenko@gmail.com>
2022-01-18 16:23:29 +0200

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ErrorPage from '../components/Error';
+
+const Error = () => {
+  return (
+    <div>
+      <ErrorPage />
+    </div>
+  );
+};
+
+export default Error;
