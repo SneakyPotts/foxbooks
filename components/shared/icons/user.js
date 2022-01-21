0d63a1dea570d5e0425cfe0c634cd499b5baee7b
Bond-Anna <anna93bondarenko@gmail.com>
2022-01-21 14:33:39 +0200

    update 12

---
+++ components/shared/icons/user.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const User = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.5444 19.3637V17.7273C18.5444 16.8593 18.1996 16.0269 17.5858 15.4132C16.9721 14.7994 16.1397 14.4546 15.2717 14.4546H8.72622C7.85824 14.4546 7.02581 14.7994 6.41205 15.4132C5.7983 16.0269 5.45349 16.8593 5.45349 17.7273V19.3637"
+        stroke="inherit"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.0014 11.1818C13.8088 11.1818 15.2741 9.71656 15.2741 7.90908C15.2741 6.1016 13.8088 4.63635 12.0014 4.63635C10.1939 4.63635 8.72864 6.1016 8.72864 7.90908C8.72864 9.71656 10.1939 11.1818 12.0014 11.1818Z"
+        stroke="inherit"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default User;
