606e130db7bbb6645e0a629944012e5ece8f8857
Bond-Anna <anna93bondarenko@gmail.com>
2022-01-31 15:41:09 +0200

    fix

--- components/shared/common/booksFilters/BookFilters.js
+++ components/shared/common/booksFilters/BookFilters.js
@@ -3,39 +3,39 @@ import { useState } from 'react';
 import classnames from 'classnames';
 
 import st from './bookFilters.module.scss';
-import {useRouter} from "next/router";
+import { useRouter } from 'next/router';
 
 const filters = [
-	{id: 1, title: 'Последние поступления', value: 1},
-	{id: 2, title: 'Популярные', value: 3},
-	{id: 3, title: 'Бестселлеры', value: 4},
-	{id: 3, title: 'Сейчас читают', value: 2}
-]
+  { id: 1, title: 'Последние поступления', value: 1 },
+  { id: 2, title: 'Популярные', value: 3 },
+  { id: 3, title: 'Бестселлеры', value: 4 },
+  { id: 3, title: 'Сейчас читают', value: 2 },
+];
 
 const BookFilters = () => {
-	const router = useRouter()
-	const [currentIdx, setCurrentIdx] = useState(+router.query['sortBy'] || 1);
+  const router = useRouter();
+  const [currentIdx, setCurrentIdx] = useState(+router.query['sortBy'] || 1);
 
-	const handleOnClick = value => {
-		router.push({query: {['sortBy']: value}}, null, {scroll: false})
-		setCurrentIdx(value);
-	};
+  const handleOnClick = value => {
+    router.push({ query: { ['sortBy']: value } }, null, { scroll: false });
+    setCurrentIdx(value);
+  };
 
-	return (
-		<div>
-			{filters?.map(i => (
-				<button
-					key={i?.id}
-					className={classnames(st.abFilter, {
-						[st.active]: currentIdx === i?.value,
-					})}
-					onClick={() => handleOnClick(i?.value)}
-				>
-					{i?.title}
-				</button>
-			))}
-		</div>
-	);
+  return (
+    <div className={st.filters}>
+      {filters?.map(i => (
+        <button
+          key={i?.id}
+          className={classnames(st.abFilter, {
+            [st.active]: currentIdx === i?.value,
+          })}
+          onClick={() => handleOnClick(i?.value)}
+        >
+          {i?.title}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default BookFilters;
