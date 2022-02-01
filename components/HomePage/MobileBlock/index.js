e2849e34bdcdb68b3bfd3b2b31ad41fcc63bfd13
Bond-Anna <anna93bondarenko@gmail.com>
2022-02-01 16:58:26 +0200

    update css

---
+++ components/HomePage/MobileBlock/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/core';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+import ShowAll from '../../shared/common/showAll/ShowAll';
+import Book from '../../shared/common/book';
+import books from '../../data/books.json';
+import ArrowRight from '../../../public/chevron-right.svg';
+import st from './mobile.module.scss';
+
+const MobileBlock = () => {
+  const { innerWidthWindow } = useSelector(state => state.common);
+  return (
+    <div>
+      <div className={st.mainBlock}>
+        <ShowAll title="Романы" url="/new" />
+
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={innerWidthWindow <= 768 ? 10 : 24}
+          slidesPerView={
+            (innerWidthWindow <= 500 && 3) || (innerWidthWindow <= 768 && 4)
+          }
+          navigation={{
+            prevEl: '.prevArrow',
+            nextEl: '.nextArrow',
+          }}
+          // onSlideChange={() => console.log('slide change')}
+          // onSwiper={swiper => console.log(swiper)}
+        >
+          {books.map(book => (
+            <SwiperSlide key={book.id} className={st.swiperSlide}>
+              <Book count={innerWidthWindow <= 500 ? 1 : 5} book={book} />
+            </SwiperSlide>
+          ))}
+          <button className="prevArrow">
+            <ArrowRight className="arrowNext" />
+          </button>
+          <button className="nextArrow">
+            <ArrowRight className="arrowNext" />
+          </button>
+        </Swiper>
+      </div>
+      <div className={st.secondBlock}>
+        <ShowAll title="Научная фантастика" url="/new" />
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={innerWidthWindow <= 768 ? 10 : 24}
+          slidesPerView={
+            (innerWidthWindow <= 500 && 3) ||
+            (innerWidthWindow <= 1024 && 4) ||
+            (innerWidthWindow >= 1200 && 5)
+          }
+          navigation={{
+            prevEl: '.prevArrow',
+            nextEl: '.nextArrow',
+          }}
+
+          // onSlideChange={() => console.log('slide change')}
+          // onSwiper={swiper => console.log(swiper)}
+        >
+          {books.map(book => (
+            <SwiperSlide key={book.id} className={st.swiperSlide}>
+              <Book count={innerWidthWindow <= 500 ? 1 : 5} book={book} />
+            </SwiperSlide>
+          ))}
+          <button className="prevArrow">
+            <ArrowRight className="arrowNext" />
+          </button>
+          <button className="nextArrow">
+            <ArrowRight className="arrowNext" />
+          </button>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default MobileBlock;
