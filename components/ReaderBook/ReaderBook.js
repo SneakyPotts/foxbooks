e6f8fd76c40f1fd940541f2b321d1f9c2af93bac
Louie787 <tigrovakyla11@gmail.com>
2022-03-01 13:51:42 +0200

    push

--- components/ReaderBook/ReaderBook.js
+++ components/ReaderBook/ReaderBook.js
@@ -17,170 +17,170 @@ import { useRouter } from 'next/router';
 import InputRange from '../shared/common/InputRange/InputRange';
 
 const ReaderBook = () => {
-	const router = useRouter()
+  const router = useRouter()
 
-	const { innerWidthWindow } = useSelector(state => state.common)
-	const { book, settings } = useSelector(state => state.reader)
+  const { innerWidthWindow } = useSelector(state => state.common)
+  const { book, settings } = useSelector(state => state.reader)
 
-	const [contentPopupIsVisible, setContentPopupIsVisible] = useState(false)
-	const [quotesPopupIsVisible, setQuotesPopupIsVisible] = useState(false)
-	const [editPopupIsVisible, setEditPopupIsVisible] = useState(false)
-	const [markPopupIsVisible, setMarkPopupIsVisible] = useState(false)
-	const [mobileControlsIsVisible, setMobileControlsIsVisible] = useState(false)
+  const [contentPopupIsVisible, setContentPopupIsVisible] = useState(false)
+  const [quotesPopupIsVisible, setQuotesPopupIsVisible] = useState(false)
+  const [editPopupIsVisible, setEditPopupIsVisible] = useState(false)
+  const [markPopupIsVisible, setMarkPopupIsVisible] = useState(false)
+  const [mobileControlsIsVisible, setMobileControlsIsVisible] = useState(false)
 
-	const showContentPopup = () => {
-		setContentPopupIsVisible(true)
-	}
+  const showContentPopup = () => {
+    setContentPopupIsVisible(true)
+  }
 
-	const showQuotesPopup = () => {
-		setQuotesPopupIsVisible(true)
-	}
+  const showQuotesPopup = () => {
+    setQuotesPopupIsVisible(true)
+  }
 
-	const toggleEditPopup = ev => {
-		ev.stopPropagation()
-		setEditPopupIsVisible(!editPopupIsVisible)
-	}
+  const toggleEditPopup = ev => {
+    ev.stopPropagation()
+    setEditPopupIsVisible(!editPopupIsVisible)
+  }
 
-	const toggleMobileControls = () => {
-		if (innerWidthWindow <= 768) {
-			setMobileControlsIsVisible(prev => !prev)
-		} else {
-			setEditPopupIsVisible(false)
-		}
-	}
+  const toggleMobileControls = () => {
+    if (innerWidthWindow <= 768) {
+      setMobileControlsIsVisible(prev => !prev)
+    } else {
+      setEditPopupIsVisible(false)
+    }
+  }
 
-	const addMark = ev => {
-		ev && ev.stopPropagation()
-		console.log('add mark');
-	}
+  const addMark = ev => {
+    ev && ev.stopPropagation()
+    console.log('add mark');
+  }
 
-	const addMarkToggler = () => {
-		if (innerWidthWindow <= 768) {
-			setMarkPopupIsVisible(true)
-		} else {
-			addMark()
-		}
-	}
+  const addMarkToggler = () => {
+    if (innerWidthWindow <= 768) {
+      setMarkPopupIsVisible(true)
+    } else {
+      addMark()
+    }
+  }
 
-	useEffect(() => {
-		return () => {
-			console.log('settings', settings)
-		}
-	}, [])
+  useEffect(() => {
+    return () => {
+      console.log('settings', settings)
+    }
+  }, [])
 
-	return (
-		<div
-			className={classNames(
-				styles.pageWrapper, 
-				styles[`brightness${settings?.screenBrightness}`]
-			)}
-			onClick={toggleMobileControls}
-		>
-			<div className={classNames('container', styles.pageContainer)}>
-				{(innerWidthWindow > 768 || mobileControlsIsVisible) &&
-					<>
-						<BackBtn
-							onClick={() => router.back()}
-							externalClass={styles.mobileBack}
-						/>
-						<Header
-							showContentPopup={showContentPopup}
-							showQuotesPopup={showQuotesPopup}
-							toggleEditPopup={toggleEditPopup}
-							addMarkToggler={addMarkToggler}
-						/>
-					</>
-				}
+  return (
+    <div
+      className={classNames(
+        styles.pageWrapper,
+        styles[`brightness${settings?.screenBrightness}`]
+      )}
+      onClick={toggleMobileControls}
+    >
+      <div className={classNames('container', styles.pageContainer)}>
+        {(innerWidthWindow > 768 || mobileControlsIsVisible) &&
+        <>
+          <BackBtn
+            onClick={() => router.back()}
+            externalClass={styles.mobileBack}
+          />
+          <Header
+            showContentPopup={showContentPopup}
+            showQuotesPopup={showQuotesPopup}
+            toggleEditPopup={toggleEditPopup}
+            addMarkToggler={addMarkToggler}
+          />
+        </>
+        }
 
-				<TextWithQoutes />
+        <TextWithQoutes />
 
-				<MyPagination
-					externalClass={styles.pagination}
-					lastPage={book?.pages_count}
-				/>
+        <MyPagination
+          externalClass={styles.pagination}
+          lastPage={book?.pages_count}
+        />
 
-				{(innerWidthWindow > 768 || mobileControlsIsVisible) &&
-					<div className={styles.progress}>
-						<div className={styles.progressWrapper}>
-							<span>Глава 1. Мальчик, который выжил</span>
-							<span>1%</span>
-						</div>
-						<InputRange
-							value={20}
-							barColor={'var(--controls-color)'}
-							externalClass={styles.progressInput}
-						/>
-					</div>
-				}
+        {(innerWidthWindow > 768 || mobileControlsIsVisible) &&
+        <div className={styles.progress}>
+          <div className={styles.progressWrapper}>
+            <span>Глава 1. Мальчик, который выжил</span>
+            <span>1%</span>
+          </div>
+          <InputRange
+            value={20}
+            barColor={'var(--controls-color)'}
+            externalClass={styles.progressInput}
+          />
+        </div>
+        }
 
-				<div className={styles.mobileFooter}>
-					<div
+        <div className={styles.mobileFooter}>
+          <div
             className={styles.mobileFooterMark}
             onClick={addMark}
           >
             <BookMark />
           </div>
-					<span>{router.query?.page} из {book?.pages_count}</span>
-				</div>
+          <span>{router.query?.page} из {book?.pages_count}</span>
+        </div>
 
-				{/* Попап с главами */}
-				{innerWidthWindow > 768 ?
-					contentPopupIsVisible &&
-						<ModalWindow
-							externalClass={styles.popup}
-							onClose={() => setContentPopupIsVisible(false)}
-						>
-							<ContentPopup />
-						</ModalWindow> :
-					contentPopupIsVisible &&
-						<DrawerPopup
-							externalClass={styles.drawer}
-							onClose={() => setContentPopupIsVisible(false)}
-						>
-							<ContentPopup />
-						</DrawerPopup>
-				}
+        {/* Попап с главами */}
+        {innerWidthWindow > 768 ?
+          contentPopupIsVisible &&
+          <ModalWindow
+            externalClass={styles.popup}
+            onClose={() => setContentPopupIsVisible(false)}
+          >
+            <ContentPopup />
+          </ModalWindow> :
+          contentPopupIsVisible &&
+          <DrawerPopup
+            externalClass={styles.drawer}
+            onClose={() => setContentPopupIsVisible(false)}
+          >
+            <ContentPopup />
+          </DrawerPopup>
+        }
 
-				{/* Попап с цитатами */}
-				{innerWidthWindow > 768 ?
-					quotesPopupIsVisible &&
-						<ModalWindow
-							externalClass={styles.popup}
-							onClose={() => setQuotesPopupIsVisible(false)}
-						>
-							<QuotesPopup />
-						</ModalWindow> :
-					quotesPopupIsVisible &&
-						<DrawerPopup
-							externalClass={styles.drawer}
-							onClose={() => setQuotesPopupIsVisible(false)}
-						>
-							<QuotesPopup />
-						</DrawerPopup>
-				}
+        {/* Попап с цитатами */}
+        {innerWidthWindow > 768 ?
+          quotesPopupIsVisible &&
+          <ModalWindow
+            externalClass={styles.popup}
+            onClose={() => setQuotesPopupIsVisible(false)}
+          >
+            <QuotesPopup />
+          </ModalWindow> :
+          quotesPopupIsVisible &&
+          <DrawerPopup
+            externalClass={styles.drawer}
+            onClose={() => setQuotesPopupIsVisible(false)}
+          >
+            <QuotesPopup />
+          </DrawerPopup>
+        }
 
-				{/* Дропдаун с редактированием */}
-				{editPopupIsVisible &&
-					<DrawerPopup
-						externalClass={styles.editPopup}
-						onClose={() => setEditPopupIsVisible(false)}
-					>
-						<EditPopup />
-					</DrawerPopup>
-				}
+        {/* Дропдаун с редактированием */}
+        {editPopupIsVisible &&
+        <DrawerPopup
+          externalClass={styles.editPopup}
+          onClose={() => setEditPopupIsVisible(false)}
+        >
+          <EditPopup />
+        </DrawerPopup>
+        }
 
-				{/* Попап с закладками */}
-				{innerWidthWindow <= 768 && markPopupIsVisible &&
-					<DrawerPopup
-						externalClass={styles.drawer}
-						onClose={() => setMarkPopupIsVisible(false)}
-					>
-						marks
-					</DrawerPopup>
-				}
-			</div>
-		</div>
-	)
+        {/* Попап с закладками */}
+        {innerWidthWindow <= 768 && markPopupIsVisible &&
+        <DrawerPopup
+          externalClass={styles.drawer}
+          onClose={() => setMarkPopupIsVisible(false)}
+        >
+          marks
+        </DrawerPopup>
+        }
+      </div>
+    </div>
+  )
 }
 
 export default ReaderBook;
