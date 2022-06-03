ed4082e8340c6af80c7a5cc04a2bf1455826b90b
Louie787 <tigrovakyla11@gmail.com>
2022-06-03 17:45:38 +0300

    push

--- components/ReaderBook/ReaderBook.js
+++ components/ReaderBook/ReaderBook.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import ModalWindow from "../shared/common/modalWindow/ModalWindow";
 import DrawerPopup from "../shared/common/DrawerPopup";
@@ -92,12 +92,6 @@ const ReaderBook = () => {
   const handleMarkClick = page => {
     router.push({ query: { ...router.query, page } })
   }
-
-  useEffect(()=> {
-    return () => {
-      alert('yo')
-    }
-  }, [])
 
   return (
     <div
