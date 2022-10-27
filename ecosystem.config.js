817b4cd88d6b9592e493063c7783e8a7b4cd70a8
antonwebnauts <antonwebnauts@gmail.com>
2022-10-27 12:55:50 +0200

    Change ecosystem config and package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,46 @@
 module.exports = {
-  apps: [
-    {
-      name: 'LoveRead',
-      exec_mode: 'cluster',
-      instances: '1', // Or a number of instances
-      script: './node_modules/.bin/next',
-      args: 'start -p 3022'
-    }
-  ]
+    apps: [
+        {
+            name: 'ui',
+            exec_mode: 'cluster',
+            instances: 1, // Or a number of instances
+            script: './node_modules/.bin/next',
+            args: 'start',
+            env: {
+                PORT: '3020',
+            }
+        },
+ {
+            name: 'ui1',
+            exec_mode: 'cluster',
+            instances: 1, // Or a number of instances
+            script: './node_modules/.bin/next',
+            args: 'start',
+            env: {
+                PORT: '3021',
+            }
+        },
+ {
+            name: 'ui2',
+            exec_mode: 'cluster',
+            instances: 1, // Or a number of instances
+            script: './node_modules/.bin/next',
+            args: 'start',
+            env: {
+                PORT: '3022',
+            }
+        },
+ {
+            name: 'ui3',
+            exec_mode: 'cluster',
+            instances: 1, // Or a number of instances
+            script: './node_modules/.bin/next',
+            args: 'start',
+            env: {
+                PORT: '3023',
+            }
+        }
+
+
+    ]
 }
