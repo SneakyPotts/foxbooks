920eb7a4bcc013b967ac1bd0db94870ce8efd886
SneakyPotts <4udaksaha@gmail.com>
2023-01-03 22:33:17 +0200

    p.205 - hidden SNs apart Google;

--- components/shared/common/SocialNetwork/SocialNetwork.js
+++ components/shared/common/SocialNetwork/SocialNetwork.js
@@ -12,22 +12,22 @@ const SocialNetwork = ({title = true, connect = false, ClassNames}) => {
 		<div className={classnames(styles.Social, ClassNames) }>
 			{title && <h4>Войти через аккаунт социальной сети</h4>}
 			<div className={styles.SocialButtons}>
-				<div>
+				{/*<div>
 					<a href={`${API_URL}/auth/yandex`} target="_blank" rel="noreferrer"><Yandex/></a>
 					{connect && <p>Подключить</p>}
-				</div>
+				</div>*/}
 				<div>
 					<a href={`${API_URL}/auth/google`} target="_blank" rel="noreferrer"><Google/></a>
 					{connect && <p>Подключить</p>}
 				</div>
-				<div>
+				{/*<div>
 					<a href={`${API_URL}/auth/vkontakte`} target="_blank" rel="noreferrer"><VK/></a>
 					{connect && <p>Подключить</p>}
-				</div>
-				<div>
+				</div>*/}
+				{/*<div>
 					<a href={`${API_URL}/auth/odnoklassniki`} target="_blank" rel="noreferrer"><OK/></a>
 					{connect && <p>Подключить</p>}
-				</div>
+				</div>*/}
 			</div>
 		</div>
 	);
