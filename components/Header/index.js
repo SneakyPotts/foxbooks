a412e5770a2b861540b57f1ddc4e58c10acb9961
SneakyPotts <4udaksaha@gmail.com>
2023-01-26 14:45:31 +0200

    search dropDown styles fixes #7;

--- components/Header/index.js
+++ components/Header/index.js
@@ -36,7 +36,8 @@ const Header = ({ socket }) => {
   const { data: store } = useSelector(state => state.search);
 
   const [flagSettings, setFlagSettings] = useState(false);
-  const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState('');
+  const [isIos, setIsIos] = useState(false);
 
   const showAuthPopup = () => {
     dispatch(setAuthPopupVisibility(true))
@@ -58,7 +59,11 @@ const Header = ({ socket }) => {
 
   const openModal = () => {
     dispatch(showMenu(true));
-    innerWidthWindow <= 768 && document.body.classList.add('nonScroll');
+    if (innerWidthWindow <= 768) {
+      isIos
+        ? document.querySelector('#__next').classList.add('nonScroll')
+        : document.body.classList.add('nonScroll')
+    }
     innerWidthWindow <= 768 && document.body.removeAttribute('style');
   };
 
@@ -137,6 +142,8 @@ const Header = ({ socket }) => {
     }
 
     document.body.addEventListener('click', hidePopup)
+
+    setIsIos(() => Boolean(window.navigator.userAgent.match(/iPhone|iPad|iPod/i)))
 
     return () => {
       document.body.removeEventListener('click', hidePopup)
