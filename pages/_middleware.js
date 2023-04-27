b1f9179948a3012e5697876a8302a65218d25fa6
SneakyPotts <4udaksaha@gmail.com>
2023-04-27 15:00:08 +0300

    fix middleware regexp for "page=1" query parameter #2;

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,8 +6,8 @@ export function middleware(request) {
   if (url.includes('page=1') && !url.match(/page=1\d/)) {
     const newUrl = url
       .replace(/\?page=1$/, '')
-      .replace(/&page=1$/, '')
-      .replace(/\?page=1&/, '?');
+      .replace(/\?page=1&/, '?')
+      .replace(/&page=1/, '');
 
     return NextResponse.redirect(newUrl, 308);
   } else {
