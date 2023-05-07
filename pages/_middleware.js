7c6b0f51422a43360046d1e193c904e0f0d87d27
SneakyPotts <4udaksaha@gmail.com>
2023-05-07 17:53:12 +0300

    disabled middleware redirect condition for /reader;

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const { url } = request;
 
-  if (url.includes('page=1') && !url.match(/page=1\d/)) {
+  if (!url.includes('/reader') && url.includes('page=1') && !url.match(/page=1\d/)) {
     const newUrl = url
       .replace(/\?page=1$/, '')
       .replace(/\?page=1&/, '?')
