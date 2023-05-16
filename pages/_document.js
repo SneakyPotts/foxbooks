4b6acb724069b9746cd41e183e991f0e654b5302
SneakyPotts <4udaksaha@gmail.com>
2023-05-16 16:05:46 +0300

    remove lock-files from ignore; created .env.example; added attr lang="ru"; on page letters changed aside categories list;

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang={'ru'}>
       <Head>
         <link rel="shortcut icon" href="/logo.ico" />
         <link rel="apple-touch-icon" sizes="192x192" href="/192x192-logo.png" />
