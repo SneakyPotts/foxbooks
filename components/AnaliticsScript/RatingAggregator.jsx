f592ef182a182f99cdbb1a747f5dd82421039b0b
SneakyPotts <4udaksaha@gmail.com>
2023-12-25 13:47:21 +0200

    change Novelties page_slug for banner;

--- components/AnaliticsScript/RatingAggregator.jsx
+++ components/AnaliticsScript/RatingAggregator.jsx
@@ -1,25 +1,42 @@
 import React, { memo } from 'react';
 
+import { JsonLd } from 'next-seo/lib/jsonld/jsonld';
+
 const RatingAggregator = memo(function RatingAggregator({ rating = 0, ratingCount = 0 }) {
   return (
-    <script
-      type="application/ld+json"
-      className="reviews-schema"
-    >
-      {`
-          {
-            "@context": "http://schema.org",
-            "@type": "Product",
-            "name": "Star rating snippet aggregator",
-            "aggregateRating": {
-              "@type": "AggregateRating",
-              "ratingValue": "${rating}",
-              "bestRating": "5",
-              "reviewCount": "${ratingCount}"
-            }
-          }
-        `}
-    </script>
+    // <script
+    //   type="application/ld+json"
+    //   className="reviews-schema"
+    // >
+    //   {`
+    //       {
+    //         "@context": "http://schema.org",
+    //         "@type": "Product",
+    //         "name": "Star rating snippet aggregator",
+    //         "aggregateRating": {
+    //           "@type": "AggregateRating",
+    //           "ratingValue": "${rating}",
+    //           "bestRating": "5",
+    //           "reviewCount": "${ratingCount}"
+    //         }
+    //       }
+    //     `}
+    // </script>
+    <JsonLd
+      // Це ваш оновлений JSON-LD об'єкт без екранування символів
+      scriptKey="product"
+      openGraph={{
+        '@context': 'http://schema.org',
+        '@type': 'Product',
+        name: 'Star rating snippet aggregator',
+        aggregateRating: {
+          '@type': 'AggregateRating',
+          ratingValue: `${rating}`,
+          bestRating: '5',
+          reviewCount: `${ratingCount}`,
+        },
+      }}
+    />
   );
 });
 
