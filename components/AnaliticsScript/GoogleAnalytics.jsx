a829e50ca111e29c3c418fe0a5a51f392ca5df1b
SneakyPotts <4udaksaha@gmail.com>
2024-01-11 17:02:50 +0200

    add script #2;

--- components/AnaliticsScript/GoogleAnalytics.jsx
+++ components/AnaliticsScript/GoogleAnalytics.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Script from 'next/script';
 
 import React from 'react';
@@ -27,20 +26,19 @@ const GoogleAnalytics = () => {
         `}
       </Script>
 
-      <Head>
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
+      <Script
+        strategy="lazyOnload"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
               (function(c,l,a,r,i,t,y){
-                c[a]=c[a]function(){(c[a].q=c[a].q[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "kjnd2appk0");
             `,
-          }}
-        />
-      </Head>
+        }}
+      />
     </>
   );
 };
